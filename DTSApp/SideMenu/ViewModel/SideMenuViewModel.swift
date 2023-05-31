a75245436193d5759bb2c50de187d6c2ae59c93c
osamakhan290 <khanosama4512@gmail.com>
2023-05-31 13:37:54 +0500

    new update

--- DTSApp/SideMenu/ViewModel/SideMenuViewModel.swift
+++ DTSApp/SideMenu/ViewModel/SideMenuViewModel.swift
@@ -1,8 +1,41 @@
-//
-//  SideMenuViewModel.swift
-//  DTS
-//
-//  Created by Apple  on 23/05/2023.
-//
+
+
+
+
+
+
 
 import Foundation
+
+enum SideMenuViewModel: Int, CaseIterable {
+    case pending
+    case inspected
+    case recommended
+    case notRecommended
+    
+    var title: String {
+        switch self {
+        case .pending:
+            return "Pending"
+        case .inspected:
+            return "Inspected"
+        case .recommended:
+            return "Recommended"
+        case .notRecommended:
+            return "NotRecommended"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .pending:
+            return "pending-icon"
+        case .inspected:
+            return "inspected-icon"
+        case .recommended:
+            return "recommended-icon"
+        case .notRecommended:
+            return "not-recommended-icon"
+        }
+    }
+}
